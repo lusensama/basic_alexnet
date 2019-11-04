40ccb95a1f70e6a1dab8b83c8270f6a09e1f8b91
lusensama <lusensama@gmail.com>
2019-11-04 11:45:09 -0500

    adjust learning rate bug

--- alex.py
+++ alex.py
@@ -47,7 +47,7 @@ class BinConv2d(nn.Module): # change the name of BinConv2d
 
 
 class AlexNet(nn.Module):
-    def __init__(self, num_classes=200):
+    def __init__(self, num_classes=1000):
         super(AlexNet, self).__init__()
         self.num_classes = num_classes
         self.features = nn.Sequential(
