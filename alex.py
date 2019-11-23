d0fca6c7e7bce501b89bc1a856f90f214578e37f
lusensama <lusensama@gmail.com>
2019-11-23 16:19:12 -0500

    modified vgg_15

--- alex.py
+++ alex.py
@@ -278,6 +278,21 @@ class VGG_15(nn.Module):
             nn.Linear(4096, 1000, bias=False)  # Linear,
         )
 
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                # nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                # nn.init.constant_(m.bias, 0)
+
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
