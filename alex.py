79e121d248d04d92ebe0e230a29fe7bca9de46ed
lusensama <lusensama@gmail.com>
2019-11-25 20:06:15 -0500

    modified vgg_15

--- alex.py
+++ alex.py
@@ -226,9 +226,9 @@ def vgg_net(pretrained=False, **kwargs):
     return model
 
 
-class VGG_15(nn.Module):
+class VGG_15_avg(nn.Module):
     def __init__(self,  dr=0.1, num_classes=1000):
-        super(VGG_15, self).__init__()
+        super(VGG_15_avg, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
             nn.ReLU(),
@@ -307,14 +307,122 @@ class VGG_15(nn.Module):
         x = self.classifier(x)
         return x
 
-def vgg_15(pretrained=False, **kwargs):
+def vgg_15_avg(pretrained=False, **kwargs):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
 
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = VGG_15(**kwargs)
+    model = VGG_15_avg(**kwargs)
+    if pretrained:
+        model_path = 'model_list/vgg15_61.pth.tar'
+        print('loading pre-trained model from '+model_path)
+        # model_path = 'alexnet_XNOR_cpu.pth'
+        pretrained_model = torch.load(model_path)
+        # from collections import OrderedDict
+        # new_state_dict = OrderedDict()
+        # for k, v in pretrained_model.items():
+        #     name = k.replace(".module", "")  # remove `module.`
+        #     new_state_dict[name] = v
+        # load params
+
+        # model.load_state_dict(pretrained_model, strict=True)
+        model.features = torch.nn.DataParallel(model.features)
+        model.cuda()
+        # torch.save(model.state_dict(), 'vgg15_gpu.pth')
+        model.load_state_dict(pretrained_model['state_dict'], strict=True)
+    return model
+
+class VGG_15_max(nn.Module):
+    def __init__(self,  dr=0.1, num_classes=1000):
+        super(VGG_15_max, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+
+            nn.ReLU(),
+            nn.AvgPool2d((2, 2), (2, 2)),
+            nn.Conv2d(64, 128, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(128, 128, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+
+            nn.ReLU(),
+            nn.AvgPool2d((2, 2), (2, 2)),
+            nn.Conv2d(128, 256, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(256, 256, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(256, 256, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+
+            nn.ReLU(),
+            nn.AvgPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),  # AvgPool2d,
+            nn.Conv2d(256, 512, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+
+            nn.ReLU(),
+            nn.AvgPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),  # AvgPool2d,
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(512, 512, (3, 3), (1, 1), (1, 1), 1, 1, bias=False),
+
+            nn.ReLU(),
+            nn.AvgPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True)
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.1),
+            nn.Linear(512, 4096, bias=False),  # Linear,
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            # nn.Linear(4096, 4096, bias=False),  # Linear,
+            # nn.ReLU(),
+            # nn.Dropout(0.1),
+            nn.Linear(4096, 100, bias=False)  # Linear,
+        )
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                # nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                # nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+def vgg_15_max(pretrained=False, **kwargs):
+    r"""AlexNet model architecture from the
+    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = VGG_15_max(**kwargs)
     if pretrained:
         model_path = 'model_list/vgg15_61.pth.tar'
         print('loading pre-trained model from '+model_path)
